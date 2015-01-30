26a690fac35d8a53e8c37378c4203ae3b560b3eb
koste4 <cghr1990@gmail.com>
2015-01-29 18:21:09 -0600

    highlighting syntax

--- js/app.js
+++ js/app.js
@@ -1,21 +1,25 @@
-// Menu Revealing
-$(window).bind('scroll', function() {
+$(document).ready(function(){
+	
+	// Menu Revealing
+	$(window).bind('scroll', function() {
 
-	var pos = $('.portfolio').position().top;
+		var pos = $('.portfolio').position().top;
 
-	 if ($(window).scrollTop() >  pos) {
-	     $('header').addClass('fixed');
-	 }
-	 else {
-	     $('header').removeClass('fixed');
-	 }
+		 if ($(window).scrollTop() >  pos) {
+		     $('header').addClass('fixed');
+		 }
+		 else {
+		     $('header').removeClass('fixed');
+		 }
 
-});
+	});
 
-// Smooth Scrolling
-$('a.smooth').click(function(){
-    $('html, body').animate({
-        scrollTop: $( $(this).attr('href') ).offset().top - 30
-    }, 500);
-    return false;
+	// Smooth Scrolling
+	$('a.smooth').click(function(){
+	    $('html, body').animate({
+	        scrollTop: $( $(this).attr('href') ).offset().top - 30
+	    }, 500);
+	    return false;
+	});
+
 });
